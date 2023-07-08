5c7733996a9259c12b4432496be4fd49b208e876
Ruben <rubengres@hotmail.fr>
2023-07-08 21:11:35 +0200

    snipping done

--- Assets/Scripts/GravityGun.cs
+++ Assets/Scripts/GravityGun.cs
@@ -1,76 +1,121 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GravityGun : MonoBehaviour
 {
 
     public Camera cam;
     public float maxGrabDist = 5f;
-    public float mov_speed = 5f;
+    public float snipTreshold = 0.5f;
+
+    public Image crosshair;
+
     public LayerMask layerMask;
 
-    GameObject grabbed;
+    public GameObject grabbed;
+
+    public Sprite spriteHandOpen;
+    public Sprite spriteHandClosed;
+    public  Sprite spriteDot;
+    public Sprite spriteCisors;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+    }
+    public void ChangeOriginTo(GameObject target, Vector3 position)
+    {
+        GameObject emptyGameObject = new GameObject("tmp_movable");
+        emptyGameObject.transform.parent = target.transform.parent;
+
+        target.transform.parent = emptyGameObject.transform;
+
+        Vector3 offset = emptyGameObject.transform.position - position;
+        foreach (Transform child in emptyGameObject.transform)
+            child.transform.position += offset;
+        emptyGameObject.transform.position = position;
+    }
+
+    void updateCrosshair(bool canGrab, bool grabbed, bool cansnip) {
+        if(!grabbed && cansnip) {
+            crosshair.sprite = spriteCisors;
+        }
+        else if (!grabbed && canGrab)
+        {
+            crosshair.sprite = spriteHandOpen;
+        }
+        else if (grabbed)
+        {
+            crosshair.sprite = spriteHandClosed;
+        }
+        else
+        {
+            crosshair.sprite = spriteDot;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (grabbed) {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        bool canGrab = false;
+        bool canSnip = false;
+
+        if (grabbed)
+        {
             Plane plane = new Plane(Vector3.forward, grabbed.transform.position);
-            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
 
             float raycastDistance;
-            if(plane.Raycast(ray, out raycastDistance)) {
+            if (plane.Raycast(ray, out raycastDistance))
+            {
                 Vector3 targetPosition = ray.GetPoint(raycastDistance);
                 grabbed.transform.position = targetPosition;
 
                 //Vector3 direction = (targetPosition - transform.position).normalized;
                 //Vector3 newPosition = transform.position + direction * mov_speed * Time.deltaTime;
-            }
-        
-        }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if(grabbed) {
-                Debug.Log("ungrabbed");
-                grabbed = null;
-            }
-            else {
-                RaycastHit hit;
-                Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
-                if (Physics.Raycast(ray, out hit, maxGrabDist)) {
-
-                    Renderer renderer = hit.collider.GetComponent<Renderer>();
-                    if (renderer && renderer.material)
-                    {
-                        Vector2 pixelUV = hit.textureCoord;
-                        pixelUV.x *= renderer.material.mainTexture.width;
-                        pixelUV.y *= renderer.material.mainTexture.height;
-
-                        if (renderer.material.mainTexture is Texture2D texture)
-                        {
-                            Color pixelColor = texture.GetPixel((int)pixelUV.x, (int)pixelUV.y);
-                            Debug.Log(pixelColor);
-
-                            if (pixelColor.a == 0f)
-                            {
-                                return; // Ignore the hit if the alpha is 0
-                            }
-                        }
-                    }
-
-                    Debug.Log("grabbed");
-                    grabbed = hit.collider.gameObject;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Transform new_parent = grabbed.transform.parent;
+                    grabbed.transform.GetChild(0).parent = new_parent;
+                    Destroy(grabbed);
+                    grabbed = null;
                 }
             }
+        } 
+        else
+        {
+            RaycastHit hit;
+            canGrab = Physics.Raycast(ray, out hit, maxGrabDist, layerMask);
 
+            if (canGrab) {                
+                GameObject grabbed_candidate = hit.collider.gameObject;
+
+                if (Mathf.Abs(grabbed_candidate.transform.localPosition.z) < snipTreshold) {
+                    canSnip = true;
+                }
+
+                if (Input.GetMouseButtonDown(0)) {
+
+                    if (canSnip)
+                    {
+                        Vector3 new_pos = grabbed_candidate.transform.localPosition;
+                        new_pos.z = grabbed_candidate.GetComponent<SnipInPosition>().relative_position;
+                        grabbed_candidate.transform.localPosition = new_pos;
+                    }
+                    else
+                    {
+                        //set new origin to hit pos
+                        ChangeOriginTo(grabbed_candidate, hit.point);
+                        grabbed = grabbed_candidate.transform.parent.gameObject;
+                    }
+                }
+            }
         }
+
+        updateCrosshair(canGrab, grabbed, canSnip);
+
     }
 }
---
+++ Assets/Scripts/SnipInPosition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnipInPosition : MonoBehaviour
+{
+
+    public float relative_position;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
